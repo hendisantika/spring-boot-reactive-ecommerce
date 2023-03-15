e549ba7c8a86c4e0a1be132a652a7f891741aca3
hendisantika <hendisantika@yahoo.co.id>
2023-03-16 06:36:16 +0700

    #5 - Add MessageConfiguration ✏️📗:octocat:🐳

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/config/MessageConfiguration.java
@@ -0,0 +1,48 @@
+package com.hendisantika.springbootreactiveecommerce.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.springframework.amqp.core.Binding.DestinationType.EXCHANGE;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/16/23
+ * Time: 06:35
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class MessageConfiguration {
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
