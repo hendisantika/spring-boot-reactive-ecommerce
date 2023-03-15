30eebc67fd0c58c3bd0b624adc12ac93b80a8ca8
hendisantika <hendisantika@yahoo.co.id>
2023-03-16 06:41:02 +0700

    #10 - Add ItemLoader ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/loader/ItemLoader.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springbootreactiveecommerce.loader;
+
+import com.hendisantika.springbootreactiveecommerce.entity.Item;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/16/23
+ * Time: 06:40
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class ItemLoader {
+
+    @Bean
+    CommandLineRunner initializeItems(ItemRepository repository) {
+        return args -> {
+            repository.findAll()
+                    .flatMap(repository::delete)
+                    .blockLast();
+
+            List<Item> items = List.of(
+                    new Item("alarm clock", 19.99),
+                    new Item("Smart TV", 249.99)
+            );
+            Flux.fromIterable(items)
+                    .flatMap(repository::save)
+                    .blockLast();
+        };
+    }
+}
