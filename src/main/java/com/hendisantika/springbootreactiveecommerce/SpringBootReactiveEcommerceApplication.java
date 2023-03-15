20256b1dc4e8abfc1fd0f762c5073ac85dae2a84
hendisantika <hendisantika@yahoo.co.id>
2023-03-16 06:30:12 +0700

    #1 - Initial Commit ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/SpringBootReactiveEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootreactiveecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveEcommerceApplication.class, args);
+    }
+
+}
