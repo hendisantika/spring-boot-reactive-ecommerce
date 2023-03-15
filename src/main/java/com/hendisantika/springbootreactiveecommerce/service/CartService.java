8cc7fa9095078cfaf10e29036547d83fb313a25c
hendisantika <hendisantika@yahoo.co.id>
2023-03-16 06:43:14 +0700

    #15 - Add getItems in CartService ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootreactiveecommerce/service/CartService.java
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/service/CartService.java
@@ -1,9 +1,11 @@
 package com.hendisantika.springbootreactiveecommerce.service;
 
+import com.hendisantika.springbootreactiveecommerce.entity.Item;
 import com.hendisantika.springbootreactiveecommerce.repository.CartRepository;
 import com.hendisantika.springbootreactiveecommerce.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +23,9 @@ public class CartService {
 
     private final ItemRepository itemRepository;
     private final CartRepository cartRepository;
+
+    public Flux<Item> getItems() {
+        return itemRepository.findAll();
+    }
+
 }
