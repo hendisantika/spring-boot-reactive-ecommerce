af345b0363fce4e4d5a8345a08937b4960abed82
hendisantika <hendisantika@yahoo.co.id>
2023-03-16 06:40:37 +0700

    #9 - Add CartLoader ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/loader/CartLoader.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootreactiveecommerce.loader;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/16/23
+ * Time: 06:40
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class CartLoader {
+
+    @Bean
+    CommandLineRunner resetCart(CartRepository repository) {
+        return args -> {
+            repository.findAll()
+                    .flatMap(repository::delete)
+                    .blockLast();
+        };
+    }
+}
