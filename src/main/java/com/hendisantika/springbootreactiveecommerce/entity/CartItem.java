d8a5eeccb83224ba93a3297188800d45d250cb74
hendisantika <hendisantika@yahoo.co.id>
2023-03-16 06:39:32 +0700

    #7 - Add CartItem Entity ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/entity/CartItem.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootreactiveecommerce.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/16/23
+ * Time: 06:39
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class CartItem {
+
+    private Item item;
+    private int quantity;
+
+    public CartItem(Item item) {
+        this.item = item;
+        this.quantity = 1;
+    }
+
+    public void increment() {
+        this.quantity += 1;
+    }
+}
