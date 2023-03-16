29ec8ff3e89ca1d13ed093b52ff60cc06afc3621
hendisantika <hendisantika@yahoo.co.id>
2023-03-17 06:22:16 +0700

    #21 - Add UserLoader ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/loader/UserLoader.java
@@ -0,0 +1,47 @@
+package com.hendisantika.springbootreactiveecommerce.loader;
+
+import com.hendisantika.springbootreactiveecommerce.entity.User;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/17/23
+ * Time: 06:21
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class UserLoader {
+
+    @Bean
+    CommandLineRunner initializeUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            userRepository.findAll()
+                    .flatMap(userRepository::delete)
+                    .blockLast();
+
+            User user = User.builder()
+                    .name("jay")
+                    .password(passwordEncoder.encode("jay"))
+                    .roles(List.of("ROLE_USER"))
+                    .build();
+            User admin = User.builder()
+                    .name("admin")
+                    .password(passwordEncoder.encode("admin"))
+                    .roles(List.of("ROLE_USER", "ROLE_INVENTORY"))
+                    .build();
+            Flux.fromIterable(List.of(user, admin))
+                    .flatMap(userRepository::save)
+                    .blockLast();
+        };
+    }
+}
