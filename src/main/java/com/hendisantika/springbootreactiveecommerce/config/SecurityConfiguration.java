35a2a1ae06f1d4f52a2b7b8d47430c193453263b
hendisantika <hendisantika@yahoo.co.id>
2023-03-17 06:20:28 +0700

    #19 - Add SecurityConfiguration ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootreactiveecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/17/23
+ * Time: 06:18
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    SecurityWebFilterChain myCustomSecurityPolicy(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                        .and()
+                        .httpBasic()
+                        .and()
+                        .formLogin())
+                .csrf().disable()
+                .build();
+    }
+}
