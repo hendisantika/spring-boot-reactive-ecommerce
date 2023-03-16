578445afdea3b32b7347dad55fe286e7f38b69d3
hendisantika <hendisantika@yahoo.co.id>
2023-03-17 06:25:22 +0700

    #24 - Add AmqpItemController ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/controller/AmqpItemController.java
@@ -0,0 +1,47 @@
+package com.hendisantika.springbootreactiveecommerce.controller;
+
+import com.hendisantika.springbootreactiveecommerce.entity.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.net.URI;
+
+import static com.hendisantika.springbootreactiveecommerce.constans.MessageConstants.EXCHANGE;
+import static com.hendisantika.springbootreactiveecommerce.constans.MessageConstants.ROUTING_KEY;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/17/23
+ * Time: 06:24
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequiredArgsConstructor
+public class AmqpItemController {
+
+    private final AmqpTemplate template;
+
+    @PreAuthorize("hasRole('INVENTORY')")
+    @PostMapping("/amqp/items/add")
+    Mono<ResponseEntity<?>> addNewItem(@RequestBody Mono<Item> item) {
+        return item.subscribeOn(Schedulers.boundedElastic())
+                .flatMap(content -> {
+                    return Mono.fromCallable(() -> {
+                        template.convertAndSend(EXCHANGE, ROUTING_KEY, content);
+                        return ResponseEntity.created(URI.create("/items"))
+                                .build();
+                    });
+                });
+    }
+}
