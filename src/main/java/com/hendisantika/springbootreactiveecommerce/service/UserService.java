d40270cc32e21abde3c470ca1c9e85bf52f2ab3e
hendisantika <hendisantika@yahoo.co.id>
2023-03-17 06:24:10 +0700

    #23 - Add UserRepository ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootreactiveecommerce/service/UserService.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootreactiveecommerce.service;
+
+import com.hendisantika.springbootreactiveecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-reactive-ecommerce
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/17/23
+ * Time: 06:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService implements ReactiveUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return userRepository.findByName(username)
+                .map(user -> User.withUserDetails(user)
+                        .build());
+    }
+}
