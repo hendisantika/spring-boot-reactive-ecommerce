c997dc42981626953a083197630837c259ad508e
hendisantika <hendisantika@yahoo.co.id>
2023-03-18 06:23:52 +0700

    #42 - Add addItemToCart_toExistingCartItem_incrementsQuantity in CartServiceTest ✏️📗:octocat:

--- src/test/java/com/hendisantika/springbootreactiveecommerce/service/CartServiceTest.java
+++ src/test/java/com/hendisantika/springbootreactiveecommerce/service/CartServiceTest.java
@@ -93,4 +93,37 @@ class CartServiceTest {
                 })
                 .verifyComplete();
     }
+
+    @Test
+    void addItemToCart_toExistingCartItem_incrementsQuantity() {
+        Item alarmClock = new Item("alarm clock", 19.99);
+        alarmClock.setId("item id");
+        when(mockItemRepository.findById("item id"))
+                .thenReturn(Mono.just(alarmClock));
+
+
+        Cart sampleCart = new Cart("My cart");
+        sampleCart.setCartItems(List.of(new CartItem(alarmClock)));
+        when(mockCartRepository.findById("My cart"))
+                .thenReturn(Mono.just(sampleCart));
+
+        Cart updatedCart = new Cart("My cart");
+        CartItem updatedCartItem = new CartItem(alarmClock);
+        updatedCartItem.increment();
+        updatedCart.setCartItems(List.of(updatedCartItem));
+        when(mockCartRepository.save(any(Cart.class)))
+                .thenReturn(Mono.just(updatedCart));
+
+        cartService.addItemToCart("My cart", "item id")
+                .as(StepVerifier::create)
+                .assertNext(cart -> {
+                    List<CartItem> cartItems = cart.getCartItems();
+                    assertThat(cartItems.size(), equalTo(1));
+                    assertThat(cartItems.get(0).getQuantity(), equalTo(2));
+                    Item item = cartItems.get(0).getItem();
+                    assertThat(item.getName(), equalTo("alarm clock"));
+                    assertThat(item.getPrice(), equalTo(19.99));
+                })
+                .verifyComplete();
+    }
 }
