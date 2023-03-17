793eacdcc0a6f459b0c2738326cfd27b983c7fde
hendisantika <hendisantika@yahoo.co.id>
2023-03-18 06:25:41 +0700

    #45 - Add cart_returnsCart in HomeControllerTests ✏️📗:octocat:

--- src/test/java/com/hendisantika/springbootreactiveecommerce/controller/HomeControllerTest.java
+++ src/test/java/com/hendisantika/springbootreactiveecommerce/controller/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.hendisantika.springbootreactiveecommerce.controller;
 
+import com.hendisantika.springbootreactiveecommerce.entity.Cart;
 import com.hendisantika.springbootreactiveecommerce.entity.Item;
 import com.hendisantika.springbootreactiveecommerce.service.CartService;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.mockito.Mockito.when;
 
@@ -46,4 +48,18 @@ class HomeControllerTests {
                 .jsonPath("$[0].price").isEqualTo(19.99);
     }
 
+    @Test
+    @WithMockUser(username = "jay")
+    void cart_returnsCart() {
+        when(mockCartService.getCart("jay's Cart"))
+                .thenReturn(Mono.just(new Cart("jay's Cart")));
+
+        client.get()
+                .uri("/cart")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo("jay's Cart")
+                .jsonPath("$.cartItems").isArray();
+    }
 }
